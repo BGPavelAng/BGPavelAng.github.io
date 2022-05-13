63f1b01fa4bd194740922e293950ff62a9d9c13c
Pavel Anguiano <50802374+BGPavelAng@users.noreply.github.com>
2022-05-13 03:36:22 +0000

    Update npage.js

--- js/npage.js
+++ js/npage.js
@@ -4,35 +4,35 @@ const tml = document.getElementById('clic');
 tml.addEventListener("click", () =>{
 	document.getElementById('cover').style.display = 'block';
 
-	let count = 12;
+	let count = 10;
 	let countdw = setInterval(function(){
 		count--;
 
-	if(count === 11){
+	if(count === 9){
 		document.getElementById('consol').style.display = 'block';
 	}
 
-	if(count === 10){
+	if(count === 8){
 		document.getElementById('consol2').style.display = 'block';
 	}
 
-	if(count === 8){
+	if(count === 6){
 		document.getElementById('consol3').style.display = 'block';
 	}
 
-	if(count === 6){
+	if(count === 4){
 		document.getElementById('consol4').style.display = 'block';
 	}
 
-	if(count === 4){
+	if(count === 3){
 		document.getElementById('consol5').style.display = 'block';
 	}
 
-	if(count === 3){
+	if(count === 2){
 		document.getElementById('consol6').style.display = 'block';
 	}
 
-	if(count === 2){
+	if(count === 1){
 		document.getElementById('consol7').style.display = 'block';
 	}
 
