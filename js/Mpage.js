1a5cc558ff9ef64d9646812c84e59e615056d4ce
Pavel Anguiano <50802374+BGPavelAng@users.noreply.github.com>
2022-06-08 12:08:20 -0500

    Update Mpage.js

--- js/Mpage.js
+++ js/Mpage.js
@@ -4,39 +4,36 @@ const MoviltM = document.getElementById('lic2');
 MoviltM.addEventListener("click", () =>{
 	document.getElementById('coverM').style.display = 'block';
 
-	let count = 10;
+	let count = 8;
 	let countdw = setInterval(function(){
 		count--;
 
-	if(count === 9){
+	if(count === 6){
 		document.getElementById('consolM').style.display = 'block';
 	}
 
-	if(count === 8){
+	if(count === 5){
 		document.getElementById('consolM2').style.display = 'block';
 	}
 
-	if(count === 6){
+	if(count === 4){
 		document.getElementById('consolM3').style.display = 'block';
 	}
 
-	if(count === 4){
+	if(count === 3){
 		document.getElementById('consolM4').style.display = 'block';
 	}
 
-	if(count === 3){
+	if(count === 2){
 		document.getElementById('consolM5').style.display = 'block';
 	}
 
-	if(count === 2){
+	if(count === 1){
 		document.getElementById('consolM6').style.display = 'block';
 	}
 
-	if(count === 1){
-		document.getElementById('consolM7').style.display = 'block';
-	}
-
 	if(count === 0){
+		document.getElementById('consolM7').style.display = 'block';
 		clearInterval(countdw);
 		window.open("secur-home.html", '_self');
 	}
