4bf0a3f47e20a8cdbf4f21426764adcb25c1842c
Pavel Anguiano <50802374+BGPavelAng@users.noreply.github.com>
2022-06-09 22:24:35 +0000

    Update rv_cod.js

--- js/rv_cod.js
+++ js/rv_cod.js
@@ -4,8 +4,10 @@ elem.addEventListener('click', () =>{
 
 	if(document.getElementById('HdSw').style.display == 'none'){
 		document.getElementById('HdSw').style.display = 'block';
+		document.getElementById('but').value = 'Ocultar';
 	}else{
 		document.getElementById('HdSw').style.display = 'none';
+		document.getElementById('but').value = 'Mostrar';
 	}
 
 	
