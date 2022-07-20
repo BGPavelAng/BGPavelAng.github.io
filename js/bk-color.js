da3d062e5fc202b8eac3707bf9cf966bed2c2618
Pavel Anguiano <50802374+BGPavelAng@users.noreply.github.com>
2022-07-20 15:21:49 -0500

    Add files via upload

---
+++ js/bk-color.js
@@ -0,0 +1,25 @@
+const body = document.querySelector('body'),
+      mTogg = body.querySelector('.modo-but'),
+      barra = document.querySelector('.barra_lateral'),
+      txtnavb = document.querySelectorAll('.nav-link-color');
+      navb = document.querySelector('.navbar');
+      but_c = document.querySelector('.clk');
+      bigb = document.querySelector('.frm');
+      bigbB = document.querySelector('.frm2');
+
+mTogg.addEventListener('click', () =>{
+
+	body.classList.toggle("dark");
+	barra.classList.toggle("b_dark");
+	navb.classList.toggle("navbar_ch");
+	navb.classList.toggle("navbar-dark");
+	but_c.classList.toggle("redb");
+	bigb.classList.toggle('frmB');
+	bigbB.classList.toggle('frm2B');
+
+	for(i = 0; i < 8; i++){
+
+		txtnavb[i].classList.toggle("text-white");
+	}
+
+});
