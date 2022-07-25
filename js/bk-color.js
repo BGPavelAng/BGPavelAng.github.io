92a2808d56c71490ef2b2ab5bd6c71d897939abe
Pavel Anguiano <50802374+BGPavelAng@users.noreply.github.com>
2022-07-25 13:43:49 -0500

    Add files via upload

--- js/bk-color.js
+++ js/bk-color.js
@@ -1,11 +1,12 @@
 const body = document.querySelector('body'),
       mTogg = body.querySelector('.modo-but'),
       barra = document.querySelector('.barra_lateral'),
-      txtnavb = document.querySelectorAll('.nav-link-color');
-      navb = document.querySelector('.navbar');
-      but_c = document.querySelector('.clk');
-      bigb = document.querySelector('.frm');
-      bigbB = document.querySelector('.frm2');
+      txtnavb = document.querySelectorAll('.nav-link-color'),
+      navb = document.querySelector('.navbar'),
+      but_c = document.querySelector('.clk'),
+      bigb = document.querySelector('.frm'),
+      bigbB = document.querySelector('.frm2'),
+      expl = document.querySelector('.baseTx');
 
 mTogg.addEventListener('click', () =>{
 
@@ -16,6 +17,7 @@ mTogg.addEventListener('click', () =>{
 	but_c.classList.toggle("redb");
 	bigb.classList.toggle('frmB');
 	bigbB.classList.toggle('frm2B');
+	expl.classList.toggle('baseTxDk');
 
 	for(i = 0; i < 8; i++){
 
