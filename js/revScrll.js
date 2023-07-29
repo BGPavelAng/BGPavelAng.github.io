861150b5e914dab59b9aed65c51cee4ae776489e
Pavel Anguiano <50802374+BGPavelAng@users.noreply.github.com>
2023-07-28 21:27:36 -0600

    Add files via upload

--- js/revScrll.js
+++ js/revScrll.js
@@ -1,36 +1,9 @@
 window.addEventListener('scroll', () => {
 
 	let getRev = document.querySelectorAll('.road');
-	let getRevInfo = document.querySelectorAll('.dat-sec');
+	
 	let getRevYnN = document.querySelectorAll('.roadP');
 
-	for(i = 0; i < getRev.length; i++){
-
-		let winH = window.innerHeight;
-		let winT = getRev[i].getBoundingClientRect().top;
-		let revP = 50;
-
-		if(winT < winH - revP){
-			getRev[i].classList.add('active');
-		}else{
-			getRev[i].classList.remove('active');
-		}
-
-	}
-
-	for(i = 0; i < getRevInfo.length; i++){
-
-		let winHinfo = window.innerHeight;
-		let winTinfo = getRevInfo[i].getBoundingClientRect().top;
-		let revPinfo = 50;
-
-		if(winTinfo < winHinfo - revPinfo){
-			getRevInfo[i].classList.add('active');
-		}else{
-			getRevInfo[i].classList.remove('active');
-		}
-
-	}
 
 	for(i = 0; i < getRevYnN.length; i++){
 
